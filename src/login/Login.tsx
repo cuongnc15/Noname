c2cc61e50f2d3d00638875b04bb9f7652cf50933
cuongnc15 <cuongnc15@users.noreply.github.com>
2023-05-15 13:51:55 +0700

    login3

--- src/login/Login.tsx
+++ src/login/Login.tsx
@@ -1,21 +1,40 @@
 import { CloseOutlined, UserOutlined } from "@ant-design/icons"
-import { Input } from 'antd';
+import { Input, Space } from 'antd';
 import styles from './login.module.css'
+import { useState } from "react";
 const Login = () => {
-    
+    const [passwordVisible, setPasswordVisible] = useState(false);
     return (
     <div className={styles.login}>
         <div className={styles.loginIconClose}>
             <CloseOutlined className={styles.IconClose}/>
         </div>
         <div className={styles.loginContainer}>
-        <h3>Log in</h3>  
-        <Input
-            placeholder="Enter your username"
-            prefix={<UserOutlined className="site-form-item-icon" />}
-        />   
-        </div>
-        
+            <h3 className={styles.loginTitle}>Login</h3>
+
+            <div>
+                <label className={styles.loginLabel} htmlFor="">User Name</label>
+                <Input
+                placeholder="Enter your username"
+                prefix={<UserOutlined className="site-form-item-icon" />}
+                />
+                <p className={styles.loginNote}>*Please enter your email</p>    
+            </div>  
+            <div>
+                <label className={styles.loginLabel} htmlFor="">Password</label>
+                <br />
+                <Space direction="horizontal">
+                <Input.Password
+                    placeholder="input password"
+                    visibilityToggle={{ visible: passwordVisible, onVisibleChange: setPasswordVisible }}
+                />
+                </Space>
+                <p className={styles.loginNote}>*Please enter your password</p> 
+           </div>
+           <div>
+                <p>Not a member yet? Register</p>
+           </div>
+        </div>        
     </div>
   );
 };
