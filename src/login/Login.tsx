468ba739206c0688e22c7964ca7a5b57c4a1a2db
cuongnc15 <cuongnc15@users.noreply.github.com>
2023-05-16 17:17:29 +0700

    login3

--- src/login/Login.tsx
+++ src/login/Login.tsx
@@ -2,12 +2,44 @@ import { CloseOutlined, UserOutlined } from "@ant-design/icons"
 import { Input, Space } from 'antd';
 import styles from './login.module.css'
 import { useState } from "react";
+
 const Login = () => {
+
     const [passwordVisible, setPasswordVisible] = useState(false);
+    const [email, setEmail] = useState("")
+    const [password, setPassword] = useState("")
+    const [err, setErr] = useState({emailInput: "",
+    passwordInput: ""})
+
+    const validateForm = () => {
+        const mess = {emailInput: "",
+                    passwordInput: ""};
+        if (email.trim() === "") {
+            mess.emailInput = "*Please enter your email"
+        }
+        if (password.trim() === "") {
+            mess.passwordInput = "*Please enter your password"
+        }
+        setErr(mess);
+        if (Object.keys(mess).length > 0) {
+        return false;
+        } else return true;
+    }
+
+    const [styleIcon, setStyleIcon] = useState("") 
+    const handleMouseInIcon = () => {
+        setStyleIcon(styles.IconCloseMouse)
+    }
+    const handleMouseOut = () => {
+        setStyleIcon("")
+    }
     return (
     <div className={styles.login}>
         <div className={styles.loginIconClose}>
-            <CloseOutlined className={styles.IconClose}/>
+            <CloseOutlined className={`${styles.IconClose} ${styleIcon}`}
+                onMouseOver={handleMouseInIcon}   
+                onMouseLeave={handleMouseOut}             
+            />
         </div>
         <div className={styles.loginContainer}>
             <h3 className={styles.loginTitle}>Login</h3>
@@ -16,20 +48,24 @@ const Login = () => {
                 <label className={styles.loginLabel} htmlFor="">User Name</label>
                 <Input
                 placeholder="Enter your username"
+                value={email}
+                onChange={(event) => setEmail(event.target.value)}
                 prefix={<UserOutlined className="site-form-item-icon" />}
                 />
-                <p className={styles.loginNote}>*Please enter your email</p>    
+                <p className={styles.loginNote}>{err.emailInput}</p>    
             </div>  
             <div>
                 <label className={styles.loginLabel} htmlFor="">Password</label>
                 <br />
                 <Space direction="horizontal">
                 <Input.Password
-                    placeholder="input password"
+                    placeholder="Enter password"
+                    value={password}
+                    onChange={(event) => setPassword(event.target.value)}
                     visibilityToggle={{ visible: passwordVisible, onVisibleChange: setPasswordVisible }}
                 />
                 </Space>
-                <p className={styles.loginNote}>*Please enter your password</p> 
+                <p className={styles.loginNote}>{err.passwordInput}</p> 
            </div>
            <div>
                 <p>Not a member yet? Register</p>
