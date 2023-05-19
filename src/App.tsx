e2602927d3256bb1ecd73af607c53c72be8e123b
cuongnc15 <cuongnc15@users.noreply.github.com>
2023-05-19 09:55:54 +0700

    1

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,10 @@
-import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import Cart from './cart/Cart';
 
 function App() {
   return (
     <div className="App">
-     
+     <Cart/>
     </div>
   );
 }
